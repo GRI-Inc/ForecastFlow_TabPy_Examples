9a2f6adf807217e0da23ca474db0421a83a34e7a
Haretaro Takahashi <takahashi.seitaro@gri.jp>
2019-12-16 18:18:15 +0900

    Update comment

--- prediction_example.py
+++ prediction_example.py
@@ -4,19 +4,19 @@ import forecastflow
 from forecastflow.tabpy_support import make_prediction_schema
 import datetime
 
-# ============================
-# Fill your own parameters
+# ============================================================================
+# [Required] Fill your ForecastFlow parameters
 email = "name@example.com"
 password = "password"
 project_id = "projectid"
 model_id = "modelid"
-# ============================
+# ============================================================================
 
-# ============================
-# Change names if necessary
+# ============================================================================
+# [Optional] Change display names
 data_name = "Tableau Prep Data " + str(datetime.datetime.now())
 prediction_name = "Tableau Prep Prediction " + str(datetime.datetime.now())
-# ============================
+# ============================================================================
 
 user = forecastflow.User(email, password)
 get_output_schema = make_prediction_schema(user, project_id, model_id)
