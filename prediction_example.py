e687dc45cd81ba9e41f3b4d1d8835ae0f00f1309
Haretaro Takahashi <takahashi.seitaro@gri.jp>
2019-12-16 16:36:12 +0900

    Add comment

--- prediction_example.py
+++ prediction_example.py
@@ -1,18 +1,22 @@
-# coding: utf-8
+# Read the blog (Japanese) to figure out how to use this script.
+# https://gri-blog.hatenablog.com/entry/2019/12/09/162019
 import forecastflow
 from forecastflow.tabpy_support import make_prediction_schema
 import datetime
 
+# ============================
 # Fill your own parameters
 email = "name@example.com"
 password = "password"
 project_id = "projectid"
 model_id = "modelid"
+# ============================
 
+# ============================
 # Change names if necessary
 data_name = "Tableau Prep Data " + str(datetime.datetime.now())
 prediction_name = "Tableau Prep Prediction " + str(datetime.datetime.now())
-
+# ============================
 
 user = forecastflow.User(email, password)
 get_output_schema = make_prediction_schema(user, project_id, model_id)
